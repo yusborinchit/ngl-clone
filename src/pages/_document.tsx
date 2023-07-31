a2ae6559fa29924a2a5dcf272e570f2019fffe79
Santiago Garcia <just.b000ringsh1t@gmail.com>
2023-07-30 21:12:41 -0300

    feat: :tada: create nextjs project and basic clean up

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en" className="antialiased scroll-smooth">
+      <Head />
+      <body className="text-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
