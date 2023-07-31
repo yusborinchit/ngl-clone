0e4b0aae34ec8e1529e98df76600e0c2cdf4fa3c
Santiago Garcia <just.b000ringsh1t@gmail.com>
2023-07-30 21:26:36 -0300

    feat: :sparkles: add supabase integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
 import "@/styles/globals.css";
+import type { Session } from "@supabase/auth-helpers-nextjs";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
+
   return (
-    <div className={inter.className}>
-      <Component {...pageProps} />
-    </div>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionContextProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
+import { useUser } from "@supabase/auth-helpers-react";
+
 export default function Home() {
-  return <main>Hello World!</main>;
+  const user = useUser();
+
+  return (
+    <>
+      <header className="max-w-md px-4 py-4 mx-auto">
+        <h1 className="text-lg font-bold">NGL Clone</h1>
+      </header>
+      <main className="max-w-md px-4 mx-auto">
+        <pre className="px-2 py-1 text-white bg-gray-500 rounded">
+          {JSON.stringify(user, undefined, 4)}
+        </pre>
+      </main>
+    </>
+  );
 }
